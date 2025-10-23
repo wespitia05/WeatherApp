454354acfbb1ca7ca4b618ea7c11678d1603ffd4
william <espiw@farmingdale.edu>
2025-10-23 00:39:33 -0400

    Bro Code weather app imports

---
+++ pyqt5_weather_app.py
@@ -0,0 +1,10 @@
+# python pyqt5 weather app
+
+# provides modules used and maintained by the python interpreter
+import sys
+# used to make requests to an api
+import requests
+# provides gui components
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
+# provides functionality
+from PyQt5.QtCore import Qt
