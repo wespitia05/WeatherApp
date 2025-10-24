ad1ec1f247a2154f9236f8985f78861776a4afbc
william <espiw@farmingdale.edu>
2025-10-23 20:12:04 -0400

    Bro Code weather app initializer for weather app class and gui event loop starter

--- pyqt5_weather_app.py
+++ pyqt5_weather_app.py
@@ -8,3 +8,22 @@ import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
 # provides functionality
 from PyQt5.QtCore import Qt
+
+# this class inherits from base class QWidget
+class WeatherApp(QWidget):
+    # initializer for our class, runs after python creates the weather app
+    # sets up everything the app needs to work
+    def __init__(self):
+        # calls the parent class initializer
+        super().__init__()
+
+# when running python file directly
+if __name__ == "__main__":
+    # creates the Qt application and handles any arguments
+    app = QApplication(sys.argv)
+    # construct our weather app 
+    weather_app = WeatherApp()
+    # show our weather app 
+    weather_app.show()
+    # weather app remains visible until exited
+    sys.exit(app.exec_())
